2cc1fd41810108264fb156902c726b425cf17699
Ross Kulyk <uaross@hotmail.com>
2023-09-26 09:11:52 -0400

    updating repo with passwort.js code for handling oAuth flow with google

--- server/index.js
+++ server/index.js
@@ -1,26 +1,40 @@
-// import express from "express";
 const express = require("express");
 const app = express();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-passport.use(new GoogleStrategy());
-// passport.use(new GoogleStrategy({
-//   clientID: GOOGLE_CLIENT_ID,
-//   clientSecret: GOOGLE_CLIENT_SECRET,
-//   callbackURL: "http://www.example.com/auth/google/callback"
-// },
-// function(accessToken, refreshToken, profile, cb) {
-//   User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//     return cb(err, user);
-//   });
-// }
-// ));
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log("accessToken:", accessToken);
+    }
+    // function (accessToken, refreshToken, profile, cb) {
+    //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //     return cb(err, user);
+    //   });
+    // }
+  )
+);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
+// http://localhost:3000/auth/google/
+// callback?code=4%2F0AfJohXnYaRj2DByAEgWGZFMZs7uFACOC2RqcxjI5DKnMB4igG9yGC_yf0psO5ZBnxjBolg&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent
