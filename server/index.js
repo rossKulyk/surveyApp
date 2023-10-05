cbb0f33e7a3dcfc2bf2f2e40034071983b7cca06
Ross Kulyk <uaross@hotmail.com>
2023-10-05 13:57:28 -0400

    implementing passport into the code and code refactoring

--- server/index.js
+++ server/index.js
@@ -1,40 +1,12 @@
 const express = require("express");
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
-// app.get("/", (req, res) => {
-//   res.send({ hi: "there" });
-// });
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.GOOGLE_CLIENT_ID,
-      clientSecret: keys.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken) => {
-      console.log("accessToken:", accessToken);
-    }
-    // function (accessToken, refreshToken, profile, cb) {
-    //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //     return cb(err, user);
-    //   });
-    // }
-  )
-);
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+// require("./routes/auth").(app);
+const authRoutes = require("./routes/auth");
+authRoutes(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-// http://localhost:3000/auth/google/
-// callback?code=4%2F0AfJohXnYaRj2DByAEgWGZFMZs7uFACOC2RqcxjI5DKnMB4igG9yGC_yf0psO5ZBnxjBolg&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent
---
+++ server/services/passport.js
@@ -0,0 +1,19 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log("accessToken:", accessToken);
+      console.log("refreshToken:", refreshToken);
+      console.log("profile:", profile);
+      console.log("done:", done);
+    }
+  )
+);
