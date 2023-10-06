2e6212fccd7bdfb5d9c6d2157fa24a682ad10e75
Ross Kulyk <uaross@hotmail.com>
2023-10-06 10:08:07 -0400

    establishing session for the user with passport serialize/desirializeUser funcs

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,18 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+//serialize user out of the session
+passport.serializeUser((user, done) => {
+  done(null, user.id); //identify user who's stored in db
+});
+//deserialize user out of the session
+passport.deserializeUser((id, done) => {
+  // done(null, user.id);
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -17,6 +29,7 @@ passport.use(
       // console.log("refreshToken:", refreshToken);
       // console.log("profile:", profile);
       // console.log("done:", done);
+      //identify user who's coming from OAuth flow
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           console.log("hello, its me :))");
